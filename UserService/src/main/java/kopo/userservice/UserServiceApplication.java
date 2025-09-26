79bd0a5bfa815e8eb437e21c60044697ce64aaec
kdw03 <kdw030612@gmail.com>
2025-09-27 00:21:07 +0900

    //Pipeline test3

--- UserService/src/main/java/kopo/userservice/UserServiceApplication.java
+++ UserService/src/main/java/kopo/userservice/UserServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-//Pipeline test2
+//Pipeline test3
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
